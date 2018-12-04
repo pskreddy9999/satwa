e92685c1ae98223615271d25a1f392e45a5ee538
pskreddy9999 <pskreddy9999@gmail.com>
2018-12-04 18:13:10 +0530

    java modified programs

--- src/basic/Sample.java
+++ src/basic/Sample.java
@@ -6,7 +6,7 @@ public class Sample {
 	public static String sggsg (String[] args) {
 	String srt= "sathish";
 	String srt1= "sathish";
-	
+	System.out.println(srt1);
 	System.out.println(srt);
    return srt;
  }
